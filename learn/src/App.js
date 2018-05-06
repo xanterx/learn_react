c0e13b03d212e3ac662397d44c41d2e748279d0a
Antriksh Singh <a0s00vf@homeoffice.wal-mart.com>
2018-05-06 20:03:43 +0530

    configured in a root

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person:[
+      {name: 'Goku', age: 56 },
+      {name: 'Vegeta', age: 40 },
+      {name:'Gohan', age:12}
+    ]
+  }
+
+  switchName = (newName) => {
+    console.log('Was clicked');
+    this.setState({
+      person: [
+        { name: newName, age: 56 },
+        { name: 'Vegeta', age: 40 },
+        { name: 'Gohan', age: 12 }
+      ]
+    });
+  }
+
+  transform = (event) => {
+    console.log('Was treansformed clicked');
+    this.setState({
+      person: [
+        { name: 'goku', age: 56 },
+        { name: event.target.value, age: 40 },
+        { name: 'Gohan', age: 12 }
+      ]
+    });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>I am react app</h1>
+        <p>Helllaosd</p>
+        <button
+          style={style} 
+          onClick={this.switchName.bind(this,'Kakarot')}>Switch</button>
+        <Person 
+          name={this.state.person[0].name} 
+          age={this.state.person[0].age} />
+        <Person 
+          name={this.state.person[1].name} 
+          age={this.state.person[1].age}
+          click={this.switchName.bind(this, 'Ultra Goku')}
+          transfor = {this.transform}>I am the prince</Person>
+        <Person 
+          name={this.state.person[2].name} 
+          age={this.state.person[2].age}/>
       </div>
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'holla bitches')
     );
   }
 }
---
+++ learn/src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css'
+
+const person = (props) => {
+    return (
+        <div className="Person">
+        <p> I am funcking {props.name}, i am {props.age} year old</p>
+        <p onClick={props.click}>{props.children}</p>
+        <input type="text" onChange={props.transfor} value={props.name} />
+        </div>
+    );
+}
+
+export default person;
